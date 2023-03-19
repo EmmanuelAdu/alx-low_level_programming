269ad7d6dddce211c3e8503e11c6998ccbee018c
EmmanuelAdu <christorenzo20@gmail.com>
2023-03-19 12:35:38 -0700

    this code prints numbersfrom 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints numbers from 0 to 9
+ *
+ * Return: always when done
+ */
+
+int main(void)
+{
+	int e;
+
+	for (e = 0 ; e <= 9 ; e++)
+	putchar(e);
+putchar('\n');
+return (0);
+}
+
