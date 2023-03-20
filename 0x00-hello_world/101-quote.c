2b5c1f0fa1b9609b1e8b3acf2be37b276819b0ba
EmmanuelAdu <christorenzo20@gmail.com>
2023-03-19 18:02:45 -0700

    This prints exactly statement in c Programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this prints a statements
+ *
+ * Return: 1 always when success
+ */
+
+int main(void)
+
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
