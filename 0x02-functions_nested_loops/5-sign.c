46b0b80a337510446c7a272e8fae6e876c3fccf1
EmmanuelAdu <christorenzo20@gmail.com>
2023-03-22 12:40:06 -0700

    this prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  *print_sign - prints the sign of a number
  *@n: the int to check
- *Return: 1 and prints + if n is greater than zero
+ *Return: 0 and prints + if n is greater than zero
  *0 and prints 0 if n is zero
  *-1 and prints - if n is less than zero
  */
@@ -23,5 +23,5 @@ int print_sign(int n)
 	{
 		_putchar('-');
 	}
-	return (-1);
+	return (0);
 }
