3344d3baec24eb9630d851bf9d05cabfc88a0927
EmmanuelAdu <christorenzo20@gmail.com>
2023-03-22 19:58:23 -0700

    this function prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,10 +1,14 @@
+#include "main.h"
+
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+
 int main(void)
+
 {
-	    jack_bauer();
-	        return (0);
+jack_bauer();
+return (0);
 }
