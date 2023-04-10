b5efbcae660c8ee7b289eadb95400778c99f47aa
EmmanuelAdu <Christorenzo20@gmail.com>
2023-04-10 10:53:50 -0700

    This program prints the name of program compiler

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 
 /**
@@ -13,6 +12,6 @@
 
 int main(__attribute__((unused)) int argc, char* argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
