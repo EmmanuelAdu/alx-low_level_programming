800c0e926918e1650c51a41a06cdce0ed53b74b7
EmmanuelAdu <christorenzo20@gmail.com>
2023-04-20 16:18:54 -0700

    This code contains a newly defined dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,84 @@
-#include <stdio.h>
-     #include "dog.h"
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- *
-int main(void)
+ *_strlen - returns the length of a string
+ *@s: string to evaluate
+ *Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+
+
+
+/**
+ *_strcpy - copies the string pointed to by src
+ *including the terminating null byte (\0)
+ *to the buffer pointed to by dest
+ *@dest: pointer to the buffer in which we copy the string
+ *@src: string to be copied
+ *Return: dest on SUCCESS
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int len, i;
+len = 0;
+while (src[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
+}
+/**
+ **new_dog - gives info of a new dog
+ *@name: tells name of the dog
+ *@age: tells age of the dog
+ *@owner: tells owner of the dog
+ *Return: pointer to new_dog(SUCCESS) else, NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog;
+int len1, len2;
+len1 = _strlen(name);
+len2 = _strlen(owner);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+dog->name = malloc(sizeof(char) * (len1 + 1));
+
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+
+dog->owner = malloc(sizeof(char) * (len2 + 1));
+if (dog->owner == NULL)
+{
+free(dog);
+free(dog->name);
+return (NULL);
+}
+
+_strcpy(dog->name, name);
+_strcpy(dog->owner, owner);
+dog->age = age;
+return (dog);
+}
