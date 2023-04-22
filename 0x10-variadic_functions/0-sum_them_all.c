8c68785bced925e181ecf6cb58bb1547797d60fc
EmmanuelAdu <christorenzo20@gmail.com>
2023-04-22 16:44:42 -0700

    This function sum a given parameter of integers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdlib.h>
+
 /**
  * sum_them_all - function to prints the sum of n parameters of int
  * @n: constant number of parameters for summing
  * Return: 0 when n == 0
  * or else (sum)
  */
-
+int sum;
+unsigned int p;
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list num_add;
@@ -16,11 +19,10 @@ int sum_them_all(const unsigned int n, ...)
 	{
 	return (0);
 	}
-	unsigned int p, sum = 0;
 
 	va_start(num_add, n);
 	for (p = 0; p < n; p++)
-		sum += va_arg(num_add, int);
+		sum += va_arg(num_add, unsigned int);
 	va_end(num_add);
 
 	return (sum);
