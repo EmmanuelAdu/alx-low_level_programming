2767de44d3bc4241dd69a8b404b8e7f4ffaef083
EmmanuelAdu <christorenzo20@gmail.com>
2023-06-16 00:31:02 -0700

    This function returns a pointer to an array created

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * array_range - this function creates an array of int
+ * @min: minimum or least range of array stored
+ * @max: maximum range of array
+ * Return: pointer to array else NULL
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr; /* pointer to memory allocation */
+	int a, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1; /* size of the space for allocation */
+
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (a = 0; min <= max; a++)
+		pta[a] = min++;
+	return (ptr);
+}
