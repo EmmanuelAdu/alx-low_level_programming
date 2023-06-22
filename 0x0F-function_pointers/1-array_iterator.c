adb12afb6ef8b714c7e5f05e31221f77265fb6df
EmmanuelAdu <christorenzo20@gmail.com>
2023-06-21 17:13:37 -0700

    This function prints an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,6 +17,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	 */
 	unsigned int ptr;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (ptr = 0; ptr < size; ptr++)
 	{
 		action(array[ptr]);
