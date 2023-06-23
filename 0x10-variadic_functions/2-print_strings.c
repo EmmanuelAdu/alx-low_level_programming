c8df70d85ebc38dbe4b17df845172a7ac7fe3bdf
EmmanuelAdu <christorenzo20@gmail.com>
2023-06-22 23:40:02 -0700

    This function prints a string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - this function prints strings
+ * @separator: constant char acting as a comma
+ * @n: number of strings passed
+ * @...: variable number of arguments passed
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+
+	unsigned int i;
+
+	va_start(str, n);
+
+	for (i = 0; i < n; i++)
+	{
+		char *current_str = va_arg(str, char *);
+
+		if (current_str == NULL)
+		{
+			printf("nil\n");
+			return;
+		}
+		else
+		printf("%s", current_str);
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(str);
+}
