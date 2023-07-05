2e598883fdf0e42d334768dee240413de1453680
EmmanuelAdu <christorenzo20@gmail.com>
2023-07-05 14:11:41 -0700

    This function prints inserts a new node at an index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t));
 
-	if (new_node == NULL || head == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -42,5 +42,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = *head;
 		*head = new_node;
 	}
-	return (NULL);
+	return (new_node);
 }
