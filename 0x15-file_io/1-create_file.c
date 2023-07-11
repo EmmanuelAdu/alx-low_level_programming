a4b6965d21ad4ebd7922ba6c73f564edfbb01d60
EmmanuelAdu <christorenzo20@gmail.com>
2023-07-10 20:13:56 -0700

    This function creates a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[len] == '\0')
+	if (!text_content)
 	{
-		len++;
+		while (text_content[len] == '\0')
+		{
+			len++;
+		}
 	}
 	OP = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	R = write(OP, text_content, len);
